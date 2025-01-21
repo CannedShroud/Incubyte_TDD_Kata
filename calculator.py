b3411a210c9dac4d8f36a9497db5c5ac46631d84
CannedShroud <vyom.d58@gmail.com>
2025-01-21 12:11:32 +0530

    feat: Introduce add function signature
    Signed-off-by: CannedShroud <vyom.d58@gmail.com>

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def add(self, numbers: str) -> int:
+        """
+        Adds numbers from a given string.
+
+        Args:
+            numbers (str): A string containing numbers separated by delimiters.
+
+        Returns:
+            int: The sum of the numbers. Returns 0 if the string is empty.
+        """
+        return 0
